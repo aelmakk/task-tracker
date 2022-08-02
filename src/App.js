5e24cf215a0f7ab6a8b9b227d6176b62fcf88186
Azeddine El makkaoui <ael-makk@e3r3p9.1337.ma>
2022-08-02 14:38:23 +0100

    tasks now is part of the app component, we can use Redux to make it accessible globaly from tasks comp.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import Header from "./Components/Header"
+import { useState } from "react"
+import Tasks from "./Components/Tasks"
 import "./index"
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "Finish Task Tracker App",
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Add Style to Register Page Ft_transcendence",
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Fix Sending Form Data in register Page ft_transcendence",
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: "Update MY Resume",
+        reminder: false,
+    },
+])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,27 +1,12 @@
-import react from "react"
 import "../index.css"
 
-const Tasks = () => {
-    const tasks = [
-        {
-            id: 1,
-            text: "Finish Task Tracker App",
-        },
-        {
-            id: 2,
-            text: "Add Style to Register Page Ft_transcendence",
-        },
-        {
-            id: 3,
-            text: "Fix Sending Form Data in register Page ft_transcendence",
-        },
-        {
-            id: 4,
-            text: "Update MY Resume",
-        },
-    ]
+const Tasks = ({tasks}) => {
     return (
-        
+        tasks.map((task) => (
+                <h3 key={task.id}>
+                    {task.text}
+                </h3>
+        ))
     );
 }
 
